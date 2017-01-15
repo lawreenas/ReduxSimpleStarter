5dce8e7bc445cf4ac1c8f455cd611d059e22d06e
Laurynas Remeika <laurynas.remeika@backbase.com>
2017-01-15 09:20:40 +0200

    Introduce first reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'To Kill A Mockingbird' },
+    { title: 'No Country For Old Men' },
+    { title: 'The Catcher In The Rye' }
+  ];
+}
